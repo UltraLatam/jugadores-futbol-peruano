09798609c1f0c92727a8a9bf2e9542aee53043a1
UltraLatam <jorge_quispe@usmp.pe>
2025-04-25 20:39:40 -0500

    Proyecto base MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace jugadores_futbol_peruano.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
